670849eae2d7c98b13c58a6fe89fcf388bd98d96
hongyunshui <jianhong513@163.com>
2019-06-26 17:42:57 +0800

    aaa

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
         System.out.println("Hello World YYY YY");
         String readFilePath = "C:\\Users\\hys\\Desktop\\IP封堵\\封堵6.14\\封禁表截至目前所有合并0614.xls"; //要读取的xls文件路径
         String writeFilePath1 = "C:\\Users\\hys\\Desktop\\IP封堵\\封堵6.14\\所有IPcommand0614.txt";  // 生成的命令所在的文件
-        String columns[] = {"IP地址","告警次数","告警时间","来源","封禁状态","报告时间","序号"};
+        String columns[] = {"IP地址","告警次数","告警时间","来源","封禁状态","报告时间","序号"}; //读取的excle字段
         int alert_num = 9;  //此值是多少就代表要抽取“告警次数大于多少的IP”来生成命令
         int denyjy_num = 30; // 此值是主机组名称的起始编号“denyjyXX”
         MkCommandAddIP.mkComdToTxt(readFilePath,columns,denyjy_num,writeFilePath1,alert_num); //调用此函数即可生成命令
